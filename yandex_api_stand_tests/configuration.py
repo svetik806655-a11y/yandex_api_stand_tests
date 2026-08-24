33228c17d4efd14d19efdbcf4d52dd61c0ade006
svetik806655-a11y <svetik806655@gmail.com>
2026-08-24 21:43:57 +0300

    Add files via upload

---
+++ yandex_api_stand_tests/configuration.py
@@ -0,0 +1,12 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+# Значение должно быть скопировано из настроек или документации сервиса, к которому предоставляется доступ.
+# Пример значения: "https://api.example.com"
+URL_SERVICE = "https://79ccb989-17ac-4b81-bda0-1269770a1d79.serverhub.praktikum-services.ru"
+
+# DOC_PATH содержит путь к документации веб-сервиса.
+# Этот путь используется для формирования полного URL пути к документации, добавляя его к базовому URL сервиса.
+# В результате получится что-то вроде "https://api.example.com/docs/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
---
+++ yandex_api_stand_tests/sender_stand_request.py
@@ -0,0 +1,20 @@
+import requests
+import configuration
+import data
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_new_user(data.user_body)
+
+# Вывод HTTP-статус кода ответа на запрос
+# Код состояния указывает на результат обработки запроса сервером
+print(response.status_code)
